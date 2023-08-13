83353b0e0b5d66c6b73f8158b17b41a3fbce6094
Shinu Samuel Sunny <14753606+shinusunny@users.noreply.github.com>
2023-08-13 19:11:01 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,13 +28,13 @@ then
     DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
     DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
 
-    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_TOKEN}"
+    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
 
     if [[ "${LOCAL_TEST}" == *"true"* ]];
     then
         echo "## [TESTING] Keyword was found but no release was created."
     else
-        echo $DATA | http POST $URL | jq .
+        echo $DATA | http POST $URL "Authorization: token ${GITHUB_TOKEN}" | jq .
     fi
 # otherwise
 else
